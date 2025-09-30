5d5df06c390d2f1bb5bf3fcfb94a49d0c17a731c
moubarak1Hub <ezzyani2018ena@gmail.com>
2025-09-30 14:56:55 +0100

    Modification path & Nom Rep

---
+++ src/stats.py
@@ -0,0 +1,33 @@
+import numpy as np
+from data import Data_List
+
+#------# Calcul valeur totale du stock
+# somme=0
+Prix,Qté=1
+Val_ttl=0
+for i in Data_List:
+    for j in range(0,3):
+        # Prix_Qte=Prix * Qté
+        # somme+=Prix_Qte
+        Val_ttl=np.sum(Prix * Qté)
+print("Valeur totale du stock est: ",Val_ttl)
+print("<================================>")
+
+#------#  Prix moyen
+Moy=0
+# Cpt=0
+for i in Data_List:
+    Moy=np.average(Prix)
+
+# if Cpt!=0:
+#     Moy=somme/Cpt   
+# else:
+#     print("Erreur!")
+print("La moyenne est: ",Moy)
+
+#------#  Prix min/max
+Prix_Max=Data_List.max(key=lambda x : x[1])
+Prix_Min=Data_List.min(Key=lambda x : x[1])
+
+#------#  Produit le plus cher et le moins cher
+print(f" Le Prix maximal : {Prix_Max} \n et Le Prix minimal : {Prix_Min}")
